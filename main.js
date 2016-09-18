6ec7d1390b2ffe7bb4590be8f7afa32eb72e46af
Hurshal Patel <hurshal@imap.cc>
2016-09-18 15:38:47 -0700

    add menu state

--- main.js
+++ main.js
@@ -3,12 +3,34 @@ var height = 800;
 
 var game;
 
+var menuState = {
+    preload: function() {
+        game.load.image('menu', 'assets/menu.png');
+    },
+
+    create: function() {
+        var bg = game.add.sprite(0, 0, 'menu');
+        bg.width = width;
+        bg.height = height;
+
+        game.input.mouse.capture = true;
+    },
+
+    update: function() {
+        if (game.input.activePointer.leftButton.isDown) {
+            game.state.start('main');
+        }
+    }
+}
+
 var mainState = {
     preload: function() {
         game.load.image('ball', 'assets/ball.jpg');
     },
 
     create: function() {
+        this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
         this.ball = game.add.sprite(100, 100, 'ball');
@@ -20,9 +42,14 @@ var mainState = {
     },
 
     update: function() {
+        if (this.spaceKey.isDown) {
+            this.ball.body.x = 10;
+        }
     }
 };
 
-game = new Phaser.Game(width, height, Phaser.AUTO, '');
+game = new Phaser.Game(width, height, Phaser.AUTO, '', null, false, false);
+
+game.state.add('menu', menuState);
 game.state.add('main', mainState);
-game.state.start('main');
+game.state.start('menu');
