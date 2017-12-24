3dcc95f756d8aec2fb32adca1cd75b9a487ba6f0
Hurshal Patel <hurshal@imap.cc>
2017-12-23 18:13:53 -0800

    add basic ecs with events

--- main.js
+++ main.js
@@ -1,23 +1,69 @@
 var game;
 
-var menuState = {
-    preload: function() {
-        game.load.image('menu', 'assets/menu.png');
-    },
+function createBall() {
+    var ball = game.add.sprite(game.world.centerX, game.world.centerY, 'block');
+    ball.tag = 'ball';
+    game.physics.arcade.enable(ball);
+    ball.body.collideWorldBounds = true;
+    ball.body.velocity.x = 500;
+    ball.body.velocity.y = 500;
+    ball.body.bounce.setTo(1, 1);
 
-    create: function() {
-        var bg = game.add.sprite(0, 0, 'menu');
-        bg.width = game.world.width;
-        bg.height = game.world.height;
+    ball.collider = {
+        onCollision: new Phaser.Signal()
+    };
 
-        game.input.mouse.capture = true;
-    },
+    return ball;
+}
 
-    update: function() {
-        if (game.input.activePointer.leftButton.isDown) {
-            game.state.start('main');
+function createPaddle() {
+    var paddle = game.add.sprite(game.world.centerX, game.world.height - 40, 'block');
+    paddle.tag = 'paddle';
+    paddle.width = 200;
+    paddle.height = 32;
+    game.physics.arcade.enable(paddle);
+    paddle.body.immovable = true;
+    paddle.body.bounce.setTo(1, 1);
+
+    paddle.collider = {
+        onCollision: new Phaser.Signal()
+    };
+
+    paddle.input = {
+        onInput: new Phaser.Signal()
+    };
+
+    paddle.input.onInput.add(function(x, y) {
+        paddle.body.position.x = game.math.clamp(x - paddle.width / 2, 0, game.world.width - paddle.width);
+    }, this);
+
+    return paddle;
+}
+
+function createBlock(x, y, blockWidth, blockHeight) {
+    var block = game.add.sprite(x, y, 'block');
+    block.tag = 'block';
+    game.physics.arcade.enable(block);
+    block.body.immovable = true;
+    block.width = blockWidth;
+    block.height = blockHeight;
+
+    block.collider = {
+        onCollision: new Phaser.Signal()
+    };
+
+    block.collider.onCollision.add(function(other) {
+        if (other.tag === 'ball') {
+            block.body.immovable = false;
+            block.body.gravity.y = 100;
+        } else if (other.tag === 'paddle') {
+            block.body.velocity.y = -block.body.velocity.y;
+        } else if (other.tag === 'block') {
+            block.kill();
         }
-    }
+    }, this);
+
+    return block;
 }
 
 var mainState = {
@@ -29,61 +75,47 @@ var mainState = {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.physics.arcade.setBounds(0, 0, game.world.width, game.world.height + 100);
 
-        this.ball = game.add.sprite(game.world.centerX, game.world.centerY, 'block');
-        game.physics.arcade.enable(this.ball);
-        this.ball.body.collideWorldBounds = true;
-        this.ball.body.velocity.x = 200;
-        this.ball.body.velocity.y = 200;
-        this.ball.body.bounce.setTo(1, 1);
+        this.entities = [ createBall(), createPaddle() ];
 
-        this.paddle = game.add.sprite(game.world.centerX, game.world.height - 40, 'block');
-        this.paddle.width = 200;
-        this.paddle.height = 32;
-        game.physics.arcade.enable(this.paddle);
-        this.paddle.body.immovable = true;
-        this.paddle.body.bounce.setTo(1, 1);
-
-        this.blocks = game.add.group();
-        var numRows = 5;
-        var maxBlockRowSize = 6;
-        var blockHeight = 64;
-        var blockMargin = 10;
-        for (var i = 0; i < numRows; i++) {
-            var y = i * (blockHeight + blockMargin) + blockMargin;
-            var rowSize = i % 2 === 1 ? maxBlockRowSize - 1 : maxBlockRowSize;
-
-            var blockWidth = (game.world.width - (maxBlockRowSize + 1) * blockMargin) / maxBlockRowSize;
-
-            var rowOffset = (i % 2 === 1 ? (blockWidth + blockMargin) / 2 : 0) + blockMargin;
-
-            for (var j = 0; j < rowSize; j++) {
-                var x = rowOffset + (blockWidth + blockMargin) * j;
-                var block = game.add.sprite(x, y, 'block');
-                game.physics.arcade.enable(block);
-                block.body.immovable = true;
-                block.width = blockWidth;
-                block.height = blockHeight;
-                this.blocks.add(block);
+        for (var i = 0; i < game.world.width; i += 110) {
+            for (var j = game.world.height / 2; j > 0; j -= 74) {
+                this.entities.push(createBlock(i, j, 100, 64));
             }
         }
     },
 
     update: function() {
-        this.paddle.body.position.x = game.math.clamp(game.input.x - this.paddle.width / 2, 0, game.world.width - this.paddle.width);
+        //input system
+        for (var i = 0; i < this.entities.length; i++) {
+            var e = this.entities[i];
 
-        game.physics.arcade.collide(this.paddle, this.ball);
-        game.physics.arcade.collide(this.ball, this.blocks, function(ball, block) {
-            block.kill();
-        });
+            // ignore non inputtable entities
+            if (!e.input) continue;
 
-        if (this.ball.body.position.y > game.world.height + this.ball.height) {
-            game.state.start('main');
+            e.input.onInput.dispatch(game.input.x, game.input.y);
+        }
+
+
+        //collision system
+        for (var i = 0; i < this.entities.length; i++) {
+            for (var j = i + 1; j < this.entities.length; j++) {
+                var a = this.entities[i];
+                var b = this.entities[j];
+
+
+                // ignore non collidable pairs
+                if (!a.collider || !b.collider) continue;
+
+                if (game.physics.arcade.collide(a, b)) {
+                    a.collider.onCollision.dispatch(b);
+                    b.collider.onCollision.dispatch(a);
+                }
+            }
         }
     }
 };
 
 game = new Phaser.Game(1200, 800, Phaser.AUTO, '', null, false, false);
 
-game.state.add('menu', menuState);
 game.state.add('main', mainState);
-game.state.start('menu');
+game.state.start('main');
